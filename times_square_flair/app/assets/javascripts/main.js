09ec86c166c0d9d1c7d9edbaf53435153128f660
Christopher King <chrisking1@gmail.com>
2016-07-31 13:40:42 -0400

    add event listener on streetview button to pass data to the map

--- times_square_flair/app/assets/javascripts/main.js
+++ times_square_flair/app/assets/javascripts/main.js
@@ -1,22 +1,34 @@
 $( document ).ready(function() {
   console.log( "ready!" );
 
-
+let myName;
+let mySqFootage;
+let myAddress;
 
 
 
 
 function listSigns(myData){
+  console.log('listSigns running!');
   for (i in myData) {
-    $('<div/>').addClass("row").attr("id", "row-number-" + i).appendTo($('#signs-list-container'))
-      $('<div/>').addClass("three columns").attr("id", "sign-name-number-" + i).appendTo($('#row-number-' + i))
-        $('<p/>').text(myData[i].screen_name_led_vinyl_signs_).appendTo($('#sign-name-number-' + i))
-      $('<div/>').addClass("three columns").attr("id", "square-footage-number-" + i).appendTo($('#row-number-' + i))
-        $('<p/>').text(myData[i].sf).appendTo($('#square-footage-number-' + i))
-      $('<div/>').addClass("three columns").attr("id", "address-number-" + i).appendTo($('#row-number-' + i))
-        $('<p/>').text(myData[i].building_address).appendTo($('#address-number-' + i))
-      $('<div/>').addClass("three columns").attr("id", "button-number-" + i).appendTo($('#row-number-' + i))
-        $('<button/>').addClass("streetview-button").text('Streetview').appendTo($('#button-number-' + i))
+    $('<div/>').addClass("row").attr("id", "row-number-" + i).appendTo($('#signs-list-container'));
+      $('<div/>').addClass("three columns").attr("id", "sign-name-number-div" + i).appendTo($('#row-number-' + i));
+        $('<p/>').text(myData[i].screen_name_led_vinyl_signs_).val(myData[i].screen_name_led_vinyl_signs_).attr("id", "sign-name-number-p" + i).appendTo($('#sign-name-number-div' + i));
+      $('<div/>').addClass("three columns").attr("id", "square-footage-number-div" + i).appendTo($('#row-number-' + i));
+        $('<p/>').text(myData[i].sf).val(myData[i].sf).attr("id", "square-footage-number-p" + i).appendTo($('#square-footage-number-div' + i));
+      $('<div/>').addClass("three columns").attr("id", "address-number-div" + i).appendTo($('#row-number-' + i));
+        $('<p/>').text(myData[i].building_address).val(myData[i].building_address).attr("id", "address-number-p" + i).appendTo($('#address-number-div' + i));
+      $('<div/>').addClass("three columns").attr("id", "button-number-div" + i).appendTo($('#row-number-' + i));
+        $('<button/>').addClass("streetview-button").attr('id', 'streetview-button-' + i).attr('name', myData[i].screen_name_led_vinyl_signs_).attr('square-footage', myData[i].sf).attr('address',myData[i].building_address).text('Streetview')
+          .on('click', function(){
+            myName      = $(this).attr('name');
+            mySqFootage = $(this).attr('square-footage');
+            myAddress   = $(this).attr('address');
+            console.log('myName is: ' + myName);
+            console.log('mySqFootage is: ' + mySqFootage);
+            console.log('myAddress is: ' + myAddress);
+          })
+          .appendTo($('#button-number-div' + i));
   };
   createMap();
 }
@@ -39,11 +51,15 @@ function getSign () {
 
 
 //Streetview button on click inside a function called in the getSign success function
-function createMap(){
-  $('.streetview-button').on("click", function() {
+function createMap(name, sqfootage, address){
     $('<button/>').attr('id','add-db-button').text('Add to my list').appendTo('#button-div');
     $('#add-db-button').on('click', function(){
       //function to add to db here
+      $('<form/>').attr('id', 'db-form').appendTo('#add-db-button');
+      console.log('Add DB button clicked!');
+      // $('<input>').attr('name', '').appendTo('#db-form');
+      // $('<input>').attr('sqfootage', '').appendTo('#db-form');
+      // $('<input>').attr('address', '').appendTo('#db-form');
     });
 
     //Relevant Google Maps code from here: https://github.com/apneadiving/Google-Maps-for-Rails
@@ -85,7 +101,6 @@ function createMap(){
     );
 
     //End of code taken from Google Maps for Rails
-  });
 };
 
 getSign();
